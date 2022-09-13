bb24dc6274cea09c39e5e7015ada8a2221020af6
Salah3422 <salah.zahran03@gmail.com>
2022-09-13 12:36:57 -0400

    third commit, front-end complete, making back-end adjustments

--- website/__init__.py
+++ website/__init__.py
@@ -12,3 +12,4 @@ def create_app():
 
     return app
 
+
--- website/views.py
+++ website/views.py
@@ -1,3 +1,4 @@
+from urllib.request import Request
 from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
@@ -16,7 +17,18 @@ def experience():
 def projects():
     return render_template("projects.html")
 
-
-@views.route('/contact')
+@views.route("/contact", methods=['GET', 'POST'])
 def contact():
-    return render_template("contact.html")
+        return render_template("contact.html")
+
+
+
+'''
+@views.route("/contact/<text>", text="Thank you!", methods=['GET', 'POST'])
+def contact(text):
+    if Request.method == "POST":
+        return render_template("/contact/<text>")
+    else:
+        return render_template("contact.html")
+'''  
+
